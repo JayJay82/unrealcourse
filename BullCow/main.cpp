66e1becaa02b35344fea354e2606aaecaffb9b9f
Costantino Cavallo <costantinocavallo@gmail.com>
2016-12-12 16:57:55 +0100

    Kick Off Course

---
+++ BullCow/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include "FBullCowGame.h"
+
+void PlayGame();
+void PrintInfo();
+std::string GetGuess();
+bool AskToPlayAgain();
+FBullCowGame BCGame;
+
+int main() 
+{
+	PrintInfo();
+	do {
+		PlayGame();
+	}
+	while(AskToPlayAgain());
+	return 0;
+}
+
+void PlayGame()
+{
+	int maxTry = BCGame.GetMaxTries();
+	std::cout << maxTry << std::endl;
+	constexpr int Max_Turn = 0;
+	for (int i = 0; i <= maxTry; i++) {
+		std::string guess = GetGuess();
+		std::cout << "Your Guess is: " << guess << std::endl;
+	}
+}
+
+std::string GetGuess()
+{
+	std::string Guess = "";
+	std::cout << "Try " << BCGame.GetCurrentTry() << ". Enter your guess: ";
+	std::getline(std::cin, Guess);
+	return Guess;
+}
+
+bool AskToPlayAgain()
+{
+	std::cout << "Do you want to play Again?: ";
+	std::string Response = "";
+	std::getline(std::cin, Response);
+	return (Response[0] == 'y' || Response[0] == 'Y');
+}
+
+void PrintInfo() 
+{
+	constexpr int WORLD_LENGHT = 9;
+	std::cout << "Bulls and Cows, a fun word game.\n";
+	std::cout << "can you guess the " << WORLD_LENGHT;
+	std::cout << " letter isogram I'm thinking of?\n";
+	return;
+}
